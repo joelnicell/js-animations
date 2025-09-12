2179a2d1b9302d91dc3da3babf288c471de98c2c
joelnicell <jjnicell@gmail.com>
2025-09-12 17:57:16 +0100

    fleshing out example one

--- script.js
+++ script.js
@@ -1,9 +1,12 @@
-const box = document.getElementById("box");
+const boxOne = document.getElementById("box-eg-one");
+const buttonOne = document.getElementById("button-eg-one");
+
 
 let value = 0;
 function animate(t) {
   value = Math.sin(t * 0.001) * 40 + 100;
-  box.style.width =  `${value}px`;
+  boxOne.style.width = `${value}px`;
+  buttonOne.style.transform = `translateX(${value}px)`;
   requestAnimationFrame((t) => animate(t))
 }
 
